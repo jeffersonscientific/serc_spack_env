c1a6162779d3a0816ed13af328fb71b2a040dbe8
Mark <mark.yoder@gmail.com>
2021-08-26 00:03:57 -0700

    added defaults/config.yaml some mods to install.sh

--- install.sh
+++ install.sh
@@ -16,17 +16,21 @@ if [[ ! -z $2 ]]; then
 	NPES=$2
 fi
 if [[ ! -z ${SLURM_CPUS_PER_TASK} ]]; then
-	NPES=$${SLURM_CPUS_PER_TASK}
+	NPES=${SLURM_CPUS_PER_TASK}
 fi
 
 # This is the general install script for SERC on the Sherlock HPC system @ Stanford.
 # 
-#
-#
 #set -x #debug
 
 
 #global variables
+# NOTE: gcc@10.1.0 might be a little buggy and 11.2 is now the 'recommended' version,
+# so let's give it a go!
+#GCC_VER="10.1.0"
+GCC_VER="11.2.0"
+INTEL_VER="2021.2.0"
+#
 CORECOUNT=${NPES} #main core count for compiling jobs
 ARCH="x86_64" #this is the main target, select either x86_64, zen2, or skylake_avx512
 if [[ ! -z $1 ]]; then
@@ -34,12 +38,13 @@ if [[ ! -z $1 ]]; then
 fi
 
 echo "*** Building for ARCH=${ARCH}; CPUs=${CORECOUNT}"
-exit 1
+#exit 1
 
 #the compilers we will need.
+#     %intel@${INTEL_VER}
 compilers=(
-    %gcc@10.1.0
-    %intel@2021.2.0
+    %intel@${INTEL_VER}
+    %gcc@${GCC_VER}
 )
 
 mpis=(
@@ -58,16 +63,27 @@ mpis=(
 
 
 #clone the spack repo into this current directory
-git clone https://github.com/spack/spack.git
+if [[ ! -d "spack" ]]; then
+	git clone https://github.com/spack/spack.git
+fi
 
 
-#backup old yaml files
-mv spack/etc/spack/defaults/packages.yaml spack/etc/spack/defaults/packages.yaml_bak
-mv spack/etc/spack/defaults/modules.yaml spack/etc/spack/defaults/modules.yaml_bak
+for fl in packages.yaml modules.yaml config.yaml
+do
+	#backup old yaml files
+	mv spack/etc/spack/defaults/${fl} spack/etc/spack/defaults/${fl}_bak
+	#
+	# copy config files:
+	cp defaults/${fl} spack/etc/spack/defaults/${fl}
+}
+done
+#mv spack/etc/spack/defaults/packages.yaml spack/etc/spack/defaults/packages.yaml_bak
+#mv spack/etc/spack/defaults/modules.yaml spack/etc/spack/defaults/modules.yaml_bak
+
 
 #copy over the configuration files:
-cp defaults/modules.yaml spack/etc/spack/defaults/modules.yaml
-cp defaults/packages.yaml spack/etc/spack/defaults/packages.yaml
+#cp defaults/modules.yaml spack/etc/spack/defaults/modules.yaml
+#cp defaults/packages.yaml spack/etc/spack/defaults/packages.yaml
 
 
 #source the spack environment from relative path
@@ -75,17 +91,17 @@ source spack/share/spack/setup-env.sh
 
 #install compilers 
 #fix was added due to zen2 not having optimizations w/ 4.8.5 compiler
-spack install -j${CORECOUNT} gcc@10.1.0%gcc@4.8.5 target=x86_64
-spack install -j${CORECOUNT} intel-oneapi-compilers@2021.2.0%gcc@4.8.5 target=x86_64
+spack install -j${CORECOUNT} gcc@${GCC_VER}%gcc@4.8.5 target=x86_64
+spack install -j${CORECOUNT} intel-oneapi-compilers@${INTEL_VER}%gcc@4.8.5 target=x86_64
 
 #now add the compilers - gcc
-spack compiler find `spack location --install-dir gcc@10.1.0`
-spack compiler find `spack location --install-dir gcc@10.1.0`/bin
+spack compiler find `spack location --install-dir gcc@${GCC_VER}`
+spack compiler find `spack location --install-dir gcc@${GCC_VER}`/bin
 
 
 #icc
-spack compiler find `spack location --install-dir  intel-oneapi-compilers`/compiler/2021.2.0/linux/bin
-spack compiler find `spack location --install-dir  intel-oneapi-compilers`/compiler/2021.2.0/linux/bin/intel64
+spack compiler find `spack location --install-dir  intel-oneapi-compilers`/compiler/${INTEL_VER}/linux/bin
+spack compiler find `spack location --install-dir  intel-oneapi-compilers`/compiler/${INTEL_VER}/linux/bin/intel64
 
 
 
@@ -118,11 +134,17 @@ do
 		spack install -j${CORECOUNT} hdf5 $compiler ^$mpi target=${ARCH}
 		spack install -j${CORECOUNT} fftw $compiler ^$mpi target=${ARCH}
 		spack install -j${CORECOUNT} parallelio $compiler ^$mpi target=${ARCH}
-		spack install -j${CORECOUNT} dealii $compiler ^$mpi target=${ARCH}
 		spack install -j${CORECOUNT} cgal $compiler ^$mpi target=${ARCH}
+		#
+		# problem children:
+		spack install -j${CORECOUNT} dealii $compiler ^$mpi target=${ARCH}
+		#  eventually, we get this error:
+		#  "%gcc@10.1.0" conflicts with "mesa" [GCC 10.1.0 has a bug]
 		#
 		# yoder:
 		spack install -j${CORECOUNT} xios $compiler ^$mpi target=${ARCH}
+		#  breaks installing Python or something? also does not seem to want to use the
+		#   already built packages; appears to be building new mpich, hdf5, and other things too.
     done
 done
 
