97c21b7e470a543ae143ccf176c3271f4e7ebbc0
Mark <mark.yoder@gmail.com>
2021-09-09 13:48:13 -0700

    adding some config files. Modifying packages.yaml. Looks like we have a working config for %intel@2021.2.0. The main thing, aside from mods on a couple of packages, appesrs to be using @2021.2.0, not @2021.3.0 -- which breaks. Still workingon the oneampi LLVM compilers. So far the main testing is done using environments; we should probably do a clean test into an empty space.

--- install.sh
+++ install.sh
@@ -15,72 +15,85 @@
 # NOTE: At this time, I think this will not parallelize across nodes, but
 #   we need to look into this more carefully.
 NPES=8
-if [[ ! -z $2 ]]; then
-	NPES=$2
-fi
+
 if [[ ! -z ${SLURM_CPUS_PER_TASK} ]]; then
 	NPES=${SLURM_CPUS_PER_TASK}
 fi
-
+if [[ ! -z $2 ]]; then
+    NPES=$2
+fi
 # This is the general install script for SERC on the Sherlock HPC system @ Stanford.
 # 
 #set -x #debug
-
-
+#
 #global variables
 # NOTE: gcc@10.1.0 might be a little buggy and 11.2 is now the 'recommended' version,
 # so let's give it a go!
 #GCC_VER="10.1.0"
 GCC_VER="11.2.0"
-INTEL_VER="2021.3.0"
+INTEL_VER="2021.2.0"
+SPACK_ENV="intel_202102"
 #
 CORECOUNT=${NPES} #main core count for compiling jobs
-
+#
 #ARCH="x86_64" #this is the main target, select either x86_64, zen2, or skylake_avx512
 ARCH=$(basename $(pwd))
 if [[ ! -z $1 ]]; then
 	ARCH=$1
 fi
-
+#
 echo "*** Building for ARCH=${ARCH}; CPUs=${CORECOUNT}"
 #exit 1
-
+#
 #the compilers we will need.
 compilers=(
     intel@${INTEL_VER}
-    gcc@${GCC_VER}
-    oneapi@${INTEL_VER}
+     gcc@${GCC_VER}
+     oneapi@${INTEL_VER}
 )
+#     gcc@${GCC_VER}
+#    oneapi@${INTEL_VER}
+#)
 #
 # MPIs... well, 
 mpis_gcc=(
-    openmpi
     mpich
+    openmpi
 )
 mpis_intel=(
-    mpich
     intel-oneapi-mpi
+    mpich
 )
-
-
+#
 #This is the ccache stuff commented out its really for testing and debugging.
 #spack install -j${CORECOUNT} ccache
 #now put ccache in the path
 #CCACHE_PATH=`spack location --install-dir ccache`
 #export PATH=$PATH:${CCACHE_PATH}/bin
-
-
-#clone the spack repo into this current directory
+#
+# if it does not exist, clone the spack repo into this current directory
 if [[ ! -d "spack" ]]; then
 	git clone https://github.com/spack/spack.git
 fi
 #
-# yoder: instead of coying the system default files, let's update the 'site' level config.
+# yoder: Removing the copy *.yaml to defaults step:
+#  1) We're better off copying to the defaults/.. level. See Spack docs, but there is a directory based hierarchy
+#   to configuration files; the recommendation is to leave 'default' alone and configure at the site, then local
+#   (~/.spack) levels. You can also use custom config hierarchies with the --config-scope= option; priority
+#   is lowest -> highest .
 #
-
 #source the spack environment from relative path
 source spack/share/spack/setup-env.sh
-
+#
+# are we using an environment?
+if [[ ! -z ${SPACK_ENV} ]]; then
+    spack env activate ${SPACK_ENV}
+    if [[ ! $?=0 ]]; then
+        echo "Error activating Spack environment: ${SPACK_ENV}"
+        exit 1
+    fi
+fi
+#
 # remove compiler config?:
 # it may be necessary to clean this out, but see below; what we really want to do is use the compiler find --scope=site option
 #rm ${HOME}/.spack/linux/compilers.yaml
@@ -93,59 +106,68 @@ spack --config-scope=config_cees/ install -j${CORECOUNT} gcc@${GCC_VER}%gcc@4.8.
 spack --config-scope=config_cees/ install -j${CORECOUNT} intel-oneapi-compilers@${INTEL_VER}%gcc@4.8.5 target=x86_64
 
 #now add the compilers - gcc
-spack --config-scope=config_cees/ compiler find --scope=site `spack location --install-dir gcc@${GCC_VER}`
-spack --config-scope=config_cees/ compiler find --scope=site `spack location --install-dir gcc@${GCC_VER}`/bin
+# use??  --config-scope=config_cees/
+spack compiler find --scope=site `spack location --install-dir gcc@${GCC_VER}`
+spack compiler find --scope=site `spack location --install-dir gcc@${GCC_VER}`/bin
 #
 #icc
-spack --config-scope=config_cees/ compiler find --scope=site `spack location --install-dir  intel-oneapi-compilers`/compiler/${INTEL_VER}/linux/bin
-spack --config-scope=config_cees/ compiler find --scope=site `spack location --install-dir  intel-oneapi-compilers`/compiler/${INTEL_VER}/linux/bin/intel64
-#
-# we might need this for some %intel installs:
-spack load gcc@${GCC_VER}
-#echo "which gcc: "
-#echo `which gcc`
-#exit 1
+spack compiler find --scope=site `spack location --install-dir  intel-oneapi-compilers`/compiler/${INTEL_VER}/linux/bin
+spack compiler find --scope=site `spack location --install-dir  intel-oneapi-compilers`/compiler/${INTEL_VER}/linux/bin/intel64
 #
 #############SOFTWARE INSTALL########################
-
+#
+# Yoder: NOTE: in principle, at least in an environment, I think all of these install statements can be shortened to:
+# spack --config-scope=config_cees/ --config-scope=config_${compiler}/ install -j${CORECOUNT}
+# At least in an environment, spack will then install everything in packages.yaml. Alternatively,
+# an environment can be installed with a spack.yaml or spack.lock
+#
 for compiler in "${compilers[@]}"
 do
 	echo "*** Installing Compiler $compiler stack..."
 	if [[ ! -d config_${compiler} ]]; then
 		mkdir config_${compiler}
 	fi
-    mpis=$mpis_gcc
+    mpis=${mpis_gcc[@]}
     if [[ ${compiler} = intel* || ${compiler} = oneapi* ]]; then
-        mpis=$mpis_intel
+        mpis=${mpis_intel[@]}
+        # load GCC?
+        # Turns out some Intel packages require a new GCC, and in fact compile components with that GCC.
+        spack load gcc@${GCC_VER}
     fi
+    #
+    echo "MPIs: ${mpis}"
+    echo "*** *** ***"
 	#
     # Serial installs
-    for pkg in proj swig maven geos intel-oneapi-tbb intel-oneapi-mkl intel-tbb
+    for pkg in proj swig geos maven intel-oneapi-tbb intel-oneapi-mkl intel-tbb
+    #for pkg in proj swig geos
     do
         spack --config-scope=config_cees/ --config-scope=config_${compiler}/ install -j${CORECOUNT} ${pkg} %${compiler} target=${ARCH}
     done
     #
     # Parallel installs
-    for mpi in "${mpis[@]}"
+    for mpi in ${mpis[@]}
     do
-		echo "*** Installing MPI elements: $compiler-$mpi"
-        spack --config-scope=config_cees/ --config-scope=config_${compiler}/ install -j${CORECOUNT} $mpi %${compiler} target=${ARCH}
+        echo "*** MPI: ${mpi}"
+        echo "*** Installing MPI dep packages: $compiler::${mpi}"
+        # install MPI implicitly from mpi dep. packages.
+        #spack --config-scope=config_cees/ --config-scope=config_${compiler}/ install -j${CORECOUNT} $mpi %${compiler} target=${ARCH}
         #
         # TODO: catch an mpi install fail. No point in going forward after that, right?
         #
         # main packages:
-        for pkg in cdo parallel-netcdf petsc netcdf-c netcdf-fortran netcdf-cxx4 hdf5 fftw parallelio cgal
+        for pkg in hdf5 netcdf-c netcdf-fortran netcdf-cxx4 cdo parallel-netcdf petsc fftw parallelio cgal
         do
             echo "*** Package: $pkg"
             spack --config-scope=config_cees/ --config-scope=config_${compiler}/ install -j${CORECOUNT} $pkg  %${compiler} ^$mpi target=${ARCH}
         done
         #
         # aux packages and problem children:
-        for pkg in freetype dealii xios
-        do
-            echo "*** Package: $pkg"
-            spack --config-scope=config_cees/ --config-scope=config_${compiler}/ install -j${CORECOUNT} $pkg  %${compiler} ^$mpi target=${ARCH}
-        done
+        #for pkg in freetype dealii xios
+        #do
+        #    echo "*** Package: $pkg"
+        #    spack --config-scope=config_cees/ --config-scope=config_${compiler}/ install -j${CORECOUNT} $pkg  %${compiler} ^$mpi target=${ARCH}
+        #done
     #
         # problem children:
         #spack install -j${CORECOUNT} dealii %${compiler} ^$mpi target=${ARCH}
